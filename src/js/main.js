c1fced088c78021a1d0e99571ff90c8d65c086ba
Igor Tarasenko <tarasenko.igr@gmail.com>
2019-03-07 16:51:55 +0200

    add full website

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,19 @@
+function mobile () {
+    return window.matchMedia('(max-width: 645px)').matches;
+}
 function animationFirst () {
     $('.second-title').addClass('slideDown');
     setTimeout(function(){
        $('.second-title').addClass('rotate');
         $('.first-title-block').addClass('slideLeft')
     }, 2000);
+}
+function animationFirstMobile () {
+    $('.second-title').addClass('rotate');
+    $('.second-title').addClass('slideDown');
+    setTimeout(function(){
+        $('.first-title-block').addClass('slideLeft')
+    }, 1000);
 }
 function animationSecond () {
     $('.third-title').addClass('slideRight');
@@ -15,10 +25,19 @@ function animationSecond () {
 function animationThird () {
     $('.last-title').addClass('slideDown');
 }
+function windowSize(){
+    if (mobile()){
+        animationFirstMobile();
+        setTimeout(animationSecond, 500);
+        setTimeout(animationThird, 5000)
+    } else {
+        animationFirst();
+        setTimeout(animationSecond, 4000);
+        setTimeout(animationThird, 8000)
+    }
+}
 $(document).ready(function () {
-    animationFirst();
-    setTimeout(animationSecond, 4000);
-    setTimeout(animationThird, 8000);
+    windowSize();
     $("a.topLink").click(function() {
         $("html, body").animate({
             scrollTop: $($(this).attr("href")).offset().top + "px"
